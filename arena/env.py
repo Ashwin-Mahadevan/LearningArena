b7f47dbd7fc7b80c244cc66de8fb9d71263c7e0f
Ashwin Mahadevan <ashwin.devan@berkeley.edu>
2022-12-28 22:12:58 -0500

    Updated environment interface to multi-agent POMDP

--- arena/env.py
+++ arena/env.py
@@ -1,21 +1,35 @@
 from abc import ABC, abstractmethod
-from typing import Hashable
+from random import Random
+
+from arena.types import Agent, AgentID
 
 
 class Environment(ABC):
+    """ Represents a multi-agent partially-observable Markov decision process. """
+
+    _agents: dict[AgentID, Agent]
+    _RNG: Random
+
+    def __init__(self, RNG: Random):
+
+        self._agents = dict()
+        self._RNG = RNG
+
+    def add_agent(self, id: AgentID, agent: Agent):
+
+        if id in self._agents:
+            raise ValueError  # TODO: Add details to exception.
+
+        self._agents[id] = agent
 
     @abstractmethod
     def reset(self):
-        ...
+        raise NotImplementedError
+
+    @abstractmethod
+    def step(self):
+        raise NotImplementedError
 
     @abstractmethod
     def is_finished(self) -> bool:
-        ...
-
-    @abstractmethod
-    def observe(self) -> Hashable:
-        ...
-
-    @abstractmethod
-    def step(self, action: Hashable) -> float:
-        ...
+        raise NotImplementedError
